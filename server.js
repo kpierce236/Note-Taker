e5060bfc22bedcdc257dcd1741078bae7e3a087c
Kevin Pierce <kpierce236@gmail.com>
2024-02-17 21:50:48 -0500

    added GET routes for the HTML files

--- server.js
+++ server.js
@@ -9,3 +9,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+// Route to return notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+  
+// Route to return index.html for all other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
