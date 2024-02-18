7fdcbdb778a70080037268ab299f08eccbcb4b7d
Kevin Pierce <kpierce236@gmail.com>
2024-02-17 21:55:10 -0500

    added endpoint to get notes from the DB file

--- server.js
+++ server.js
@@ -20,6 +20,23 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to read data from the database.' });
+        return;
+      }
+  
+      const notes = JSON.parse(data);
+      res.json(notes);
+    });
+  });
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
